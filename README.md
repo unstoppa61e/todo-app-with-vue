31d9e3df7120f9da7762fd4fc76d6b8232c7acf7
Makoto Onoue <like_a_newborn_baby@icloud.com>
2021-11-19 08:35:13 +0900

    add: resetStatus function

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # todo-app-with-vue
-Todo app with CRUD functions.
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+const additionButtonLabel = 'Add'
+const additionIndex = -1
+
 const app = Vue.createApp({
   data: () => ({
     newItem: '',
-    buttonLabel: 'Add',
-    editingIndex: -1,
+    buttonLabel: additionButtonLabel,
+    editingItemIndex: additionIndex,
     todos: []
   }),
   watch: {
@@ -16,30 +19,34 @@ const app = Vue.createApp({
   methods: {
     setItem: function() {
       const item = this.newItem
+      if (item === '') {
+        return
+      }
       if (this.isEditing()) {
-        this.todos[this.editingIndex] = this.newItem
-        this.editingIndex = -1
-        this.buttonLabel = 'Add'
+        this.todos[this.editingItemIndex] = item
       } else {
         this.todos.push(item)
       }
-      this.newItem = ''
+      this.resetStatus()
     },
     isEditing: function () {
-      return this.editingIndex !== -1
+      return this.editingItemIndex !== additionIndex
     },
     deleteItem: function(index) {
       if (confirm('Are you sure?')) {
         this.todos.splice(index, 1)
+        this.resetStatus()
       }
     },
     editItem: function(index) {
       this.newItem = this.todos[index]
-      this.editingIndex = index
-      this.setButtonText()
-    },
-    setButtonText: function () {
+      this.editingItemIndex = index
       this.buttonLabel = 'Update'
+    },
+    resetStatus: function () {
+      this.buttonLabel = additionButtonLabel
+      this.editingItemIndex = additionIndex
+      this.newItem = ''
     }
   },
   mounted: function() {
